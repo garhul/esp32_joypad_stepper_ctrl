c3e57c2b5672db1b2d75ca8cbef0eb675268af78
garhul <adrian.gesto@gmail.com>
2023-07-23 03:17:55 +0200

    increased poll freq

--- esp32_stepper_coso.ino
+++ esp32_stepper_coso.ino
@@ -4,6 +4,7 @@
 #include "motors.h"
 #include "pad.h"
 
+
 // Arduino setup function. Runs in CPU 1
 void setup() {
   Serial.begin(115200);
@@ -21,18 +22,18 @@ void loop() {
   static uint32_t joyPoll = 0;
   MOTORS::update();
   
-  if ((millis() - joyPoll) > 150) { // keep polling at 8hz (for now)
+  if ((millis() - joyPoll) > 25) { // keep polling at 40hz (for now)
     joyPoll = millis();
 
     PAD::update();
     GamepadPtr pad = PAD::getGamepad();
     if (pad != NULL) {
 
-      MOTORS::setStepperSpeed(MOTORS::STEPPERS::STEP_A, trim(pad->axisX(), 100));
-      MOTORS::setStepperSpeed(MOTORS::STEPPERS::STEP_B, trim(pad->axisY(), 100));
+      MOTORS::setStepperSpeed(MOTORS::STEPPERS::STEP_A, trim(pad->axisX(), 50));
+      MOTORS::setStepperSpeed(MOTORS::STEPPERS::STEP_B, trim(pad->axisY(), 50));
       
-      MOTORS::setServoPosition(MOTORS::SERVOS::SERVO_A, trim(pad->axisRX(), 50));
-      MOTORS::setServoPosition(MOTORS::SERVOS::SERVO_B, trim(pad->axisRY(), 50));
+      MOTORS::setServoPosition(MOTORS::SERVOS::SERVO_A, trim(pad->axisRX(), 25));
+      MOTORS::setServoPosition(MOTORS::SERVOS::SERVO_B, trim(pad->axisRY(), 25));
     
     
     } else {
